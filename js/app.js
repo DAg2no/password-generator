cc023012b631569f3917006c1160321b0697b952
DAg2no <dualwinder777.2.0@gmail.com>
2022-09-07 17:39:07 -0400

    hola

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-var hola = window.getComputedStyle(DOMImplementation, $`hola`, function(){
-    DOMMatrix.window//hola
+
+const generate = () => {
+    var length = parseInt(inputlength.value);
+    var generatedPassword = document.getElementById('generatedPassword');
+    var btnGenerate = document.getElementById('btnGenerate');
+    var checkbox1 = document.getElementById('checkbox1');
+    var checkbox2 = document.getElementById('checkbox2');
+    var letras = "abcdefghyijklmnopqrstuvwxyzABCDEFGHIJKLMNOPKRSTUVWXYZ";
+    const numeros = '0123456789';
+    const simbolos = '!"·#$¢%∞&¬/÷(“)=≠?´¿^[*]¨{Çç},;„.:…-_';
+
+    if(checkbox1.ariaChecked) letras+=numeros;
+    if(checkbox2.ariaChecked) numeros+=simbolos;
+
+
+    generatedPassword.innerText = "AAA";
+}
+
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    btnGenerate.addEventListener('click,', () =>{
+        generate();
+    })
 })
