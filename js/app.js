9b8f0622342dfd14ce8e37f877c5290a9b18f9da
DAg2no <92042414+DAg2no@users.noreply.github.com>
2022-09-07 16:49:57 -0300

    hol

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
-var hola = window.getComputedStyle(DOMImplementation, $`hola`, function(){
-    DOMMatrix.window//hola
+const generate = () => {
+    var length=parseInt(inputlength.value);
+    var letras = "abcdefghyijklmnopqrstuvwxyzABCDEFGHIJKLMNOPKRSTUVWXYZ";
+    const numeros = '0123456789';
+    const simbolos = '!"·#$¢%∞&¬/÷(“)=≠?´¿^[*]¨{Çç},;„.:…-_';
+
+    if(checkbox1.checked) letras+=numeros;
+    if(checkbox2.checked) numeros += simbolos;
+
+
+    generatePassword.innertext = "AAA";
+
+}
+
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    btnGenerate.addEventListener('click,', () =>{
+        generate();
+    })
 })
